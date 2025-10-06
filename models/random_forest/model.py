bd66f635a556277ce0c6b58ac27df01a2522230c
m3188312-commits <m3188312@gmail.com>
2025-10-06 14:25:35 +0300

    Update model.py

--- models/random_forest/model.py
+++ models/random_forest/model.py
@@ -1,13 +1,41 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 
-def get_model(n_estimators=100, max_depth=20, n_jobs=-1, random_state=42):
+def get_model(X=None, y=None, use_gridsearch=False):
+    if use_gridsearch and X is not None and y is not None:
+        param_grid = {
+        'n_estimators':[100, 200, 300],
+        'max_depth':[10],
+        'max_features':['sqrt', 'log2'],
+        'min_samples_leaf': [1,5]
+        }
+        
+        rf = RandomForestClassifier(
+             n_jobs=-1,
+             random_state =42
+        )
+
+        grid = GridSearchCV (
+             estimator=rf,
+             param_grid=param_grid,
+             cv=3,
+             scoring='f1_macro',
+             n_jobs=1,
+             verbose=2
+        )
+        grid.fit(X, y)
+
+        print("Best Parameters: ", grid.best_params_)
+        return grid.best_estimator_
+
+    #fallback: baseline model
     return RandomForestClassifier(
-        n_estimators=n_estimators,
-        max_depth=max_depth,
-        n_jobs=n_jobs,
-        random_state=random_state
+        n_estimators=100,
+        max_depth=20,
+        n_jobs=-1,
+        random_state=42
     )
 
 
@@ -19,3 +47,4 @@ def save_model(clf, path: str):
 def load_model(path: str):
     with open(path, 'rb') as f:
         return pickle.load(f)
+    
